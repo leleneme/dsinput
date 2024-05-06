e65209c0820fa77e475ca95900d76b8b784995bb
leleneme <joaolemss@gmail.com>
2024-05-05 22:31:15 -0300

    nds: refactoring code to centralize app logic

---
+++ client-nds/arm9/include/app.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "app_config.hpp"
+
+#include <dsiwifi9.h>
+
+#include <sys/socket.h>
+#include <cstdint>
+#include <string>
+
+namespace dsinput {
+
+constexpr const char* APPCONFPATH = "/data/settings/dsinput.conf";
+
+enum class app_error {
+    none,
+    invalid_port,
+    invalid_address,
+    fat_init,
+    config_not_found,
+};
+
+// Ensure casting from app_config_error to app_error is possible
+static_assert((int)app_error::invalid_address == (int)app_config_error::invalid_address);
+static_assert((int)app_error::invalid_port == (int)app_config_error::invalid_port);
+static_assert((int)app_error::none == (int)app_error::none);
+
+struct app {
+    app_config config;
+    uint32_t client_ip;
+    bool connected;
+    int sock;
+    struct sockaddr_in server_addr;
+
+    uint16_t last_keys = 0;
+
+    std::string log;
+
+    void on_connect();
+    void tick();
+    void send_data();
+    void loop();
+
+    void set_wifi_connect_cb(WifiConnectHandler handler);
+};
+
+app_error app_init(app& app);
+
+}
---
+++ client-nds/arm9/source/app.cpp
@@ -0,0 +1,102 @@
+#include <nds.h>
+#include <fat.h>
+#include <dsiwifi9.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include "app.hpp"
+#include "fs.hpp"
+
+namespace dsinput {
+
+app_error app_init(app& app) {
+    DSiWifi_InitDefault(true);
+
+    if (!fatInitDefault())
+        return app_error::fat_init;
+
+    auto conf_file_opt = file_read_to_string(APPCONFPATH);
+    if (!conf_file_opt)
+        return app_error::config_not_found;
+
+    auto conf_file = conf_file_opt.value();
+    auto conf_error = app_config_parse(app.config, conf_file);
+    if (conf_error != app_config_error::none) {
+        return static_cast<app_error>(conf_error);
+    }
+
+    return app_error::none;
+}
+
+void app::tick() {
+    uint32_t new_ip = DSiWifi_GetIP();
+    if (new_ip != this->client_ip) {
+        client_ip = new_ip;
+        if (new_ip == 0 || new_ip == 0xFFFFFFFF) {
+            connected = false;
+            return;
+        }
+    }
+
+    if (new_ip != 0 || new_ip != 0xFFFFFFFF) {
+        connected = true;
+    }
+
+    uint16_t new_keys = keysCurrent();
+    if (new_keys != last_keys && connected) {
+        send_data();
+        last_keys = new_keys;
+    }
+}
+
+#include <errno.h>
+#include <string.h>
+
+void app::on_connect() {
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    if (sock < 0) {
+        iprintf("socket() failed: %s\n", strerror(errno));
+        while (1) { }
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(config.host_port);
+    server_addr.sin_addr.s_addr = inet_addr(config.host_address.c_str());
+
+    connected = true;
+}
+
+void app::send_data() {
+    uint16_t data_be = htons(last_keys);
+    sendto(sock, &data_be, sizeof(uint16_t), 0,
+        (struct sockaddr*)&server_addr, sizeof(struct sockaddr_in));
+}
+
+static void ip_u32_to_u8_arr(uint32_t ip, uint8_t dest[4]) {
+    memcpy(dest, &ip, 4);
+}
+
+void app::loop() {
+    while (1) {
+        swiWaitForVBlank();
+        scanKeys();
+
+        tick();
+
+        consoleClear();
+        u8 addr_bytes[8];
+        ip_u32_to_u8_arr(client_ip, addr_bytes);
+
+        iprintf("\x1b[s\x1b[0;0HClient IP: %u.%u.%u.%u               ", addr_bytes[0], addr_bytes[1], addr_bytes[2], addr_bytes[3]);
+        iprintf("\x1b[s\x1b[1;0HServer Ip: %s:%d\n", config.host_address.c_str(), config.host_port);
+        iprintf("\x1b[s\x1b[2;0HConnected: %s\n", connected ? "yes" : "no");
+    }
+}
+
+void app::set_wifi_connect_cb(WifiConnectHandler handler) {
+    DSiWifi_SetConnectHandler(handler);
+}
+
+}
